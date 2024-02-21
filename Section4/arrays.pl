2910fa23a0eaa27d28cb882d36cac83f7c7a811a
JonathanJoo82 <jonathanjoo@gmail.com>
2024-02-20 19:30:13 -0600

    Create arrays.pl

---
+++ Section4/arrays.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+# list of different values in a sequential order - What is an array? 
+# contains the list of different values
+
+# @array_name = ("Jaddian", 44, "Victor", 89, "Jackson");
+
+@array_name = ("Jaddian", 44, "Victor", 89, "Jackson");
+
+
+#print("@array_name\n");
+#without the double quotes it will not include spaces in between the elements of the array. 
+
+@number_array = (1, 2, 3, 4, 5);
+@string_array = ("one", "two", "three", "four", "five");
+
+print("@array_name\n");
+print("@number_array\n");
+print("@string_array\n");
+
