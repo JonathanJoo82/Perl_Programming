1989b49b32b18440650f8ce05bdd727102aa015a
JonathanJoo82 <jonathanjoo@gmail.com>
2024-02-24 16:48:04 -0600

    asdf

---
+++ Section6/stdin.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+
+
+print("enter the first number: ");
+$x = <STDIN>;
+
+
+print("enter the second number: ");
+$y = <STDIN>;
+$results = $x + $y;
+
+print("$x + $y = $results\n");
