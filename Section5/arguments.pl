7ccabe3072e6b9a968d749072a5a95e0770173bb
JonathanJoo82 <jonathanjoo@gmail.com>
2024-02-23 22:11:38 -0600

    asdf

--- Section5/arguments.pl
+++ Section5/arguments.pl
@@ -1,5 +1,15 @@
 #!/usr/bin/perl
 
-sub print_varaible(){
-    print("hello world");
+
+sub addition(){
+    ($first, $second) = @_;
+    print("$first + $second\n");
 }
+
+sub print_name(){
+    ($firstName, $lastName) = @_;
+    print("Hello, $firstName $lastName\n")
+}
+
+&addition(1, 2);
+&print_name("Zack", "Babyface");
